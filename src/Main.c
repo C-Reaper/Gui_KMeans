48d5f49cbe1e9468f755e969463169d167ed07e9
codeleaded <gnu.c.reaper@gmail.com>
2026-03-19 16:04:33 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,32 +1,69 @@
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/TransformedView.h"
 #include "/home/codeleaded/System/Static/Library/KNN.h"
 
 #define KNN_CENTER_PATH     "./data/Output." KNN_CENTER_FILETYPE
 #define KNN_DATA_PATH       "./data/Output." KNN_DATA_FILETYPE
 #define KNN_LEARNRATE       0.5f
+#define KNN_STARTPOINTS     50
 
-int main(int argc,char** argv){
-    
-    KNN knn = KNN_New(4U,2U,3U);
-    KNN_Add_Data(&knn,(NeuralType*[]){
-        (NeuralType[]){ 0.0f,0.0f },
-        (NeuralType[]){ 0.0f,0.0f },
-        (NeuralType[]){ 0.0f,0.0f },
-        (NeuralType[]){ 0.0f,0.0f },
-        (NeuralType[]){ 0.0f,0.0f },
-        (NeuralType[]){ 0.0f,0.0f },
-        (NeuralType[]){ 0.0f,0.0f },
-        (NeuralType[]){ 0.0f,0.0f },
-        NULL
-    });
-    KNN_Load_Data(&knn,KNN_DATA_PATH);
-    
-    KNN_Relocate(&knn,KNN_LEARNRATE);
-    int c = KNN_Centerize(&knn,(NeuralType[]){ 0.0f,0.0f });
-    printf("Center: %d\n",c);
+TransformedView tv;
+KNN knn;
 
+void Setup(AlxWindow* w){
+	knn = KNN_New(4U,2U,3U);
+
+    for(int i = 0;i<KNN_STARTPOINTS;i++){
+        KNN_Add_Data(&knn,(NeuralType*[]){
+            (NeuralType[]){ Random_f64_New(),Random_f64_New() },
+            NULL
+        });
+    }
+    //KNN_Load_Data(&knn,KNN_DATA_PATH);
     //KNN_Save(&knn,KNN_CENTER_PATH);
     //KNN_Load(&knn,KNN_CENTER_PATH);
     
+	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
+}
+void Update(AlxWindow* w){
+    tv.ZoomSpeed = 1.0f * w->ElapsedTime;
+	TransformedView_HandlePanZoom(&tv,window.Strokes,GetMouse());
+
+    if(Stroke(ALX_MOUSE_R).PRESSED){
+        KNN_Relocate(&knn,KNN_LEARNRATE);
+    }
+    if(Stroke(ALX_MOUSE_L).PRESSED){
+        Vec2 const p = TransformedView_ScreenWorldPos(&tv,GetMouse());
+        KNN_Add_Data(&knn,(NeuralType*[]){
+            (NeuralType[]){ p.x,p.y },
+            NULL
+        });
+    }
+
+	Clear(BLACK);
+
+	Vec2 const sp = TransformedView_WorldScreenPos(&tv,(Vec2){ 0.0f,0.0f });
+	Vec2 const sd = TransformedView_WorldScreenLength(&tv,(Vec2){ 1.0f,1.0f });
+	Rect_RenderXWire(WINDOW_STD_ARGS,sp,sd,WHITE,1.0f);
+
+	Vec2 const ntd = TransformedView_WorldScreenLength(&tv,(Vec2){ 0.01f,0.01f });
+    for(int i = 0;i<knn.dps.size;i++){
+        NeuralType* nt = *(NeuralType**)Vector_Get(&knn.dps,i);
+        const int c = KNN_Centerize(&knn,nt);
+        Pixel color = WHITE;
+        if(c == 0)      color = RED;
+        else if(c == 1) color = GREEN;
+        
+        Vec2 const ntp = TransformedView_WorldScreenPos(&tv,(Vec2){ nt[0],nt[1] });
+        Circle_R_RenderWire(WINDOW_STD_ARGS,ntp,ntd,color,1.0f);
+    }
+}
+void Delete(AlxWindow* w){
     KNN_Free(&knn);
+}
+
+int main(){
+    if(Create("KNN",1900,1100,1,1,Setup,Update,Delete))
+        Start();
     return 0;
 }
