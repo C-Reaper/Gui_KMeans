444282d07bfa4cb606f6b1b19dc963f6a7f9d265
codeleaded <gnu.c.reaper@gmail.com>
2026-06-01 02:47:00 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -12,7 +12,7 @@ TransformedView tv;
 KMeans knn;
 
 void Setup(AlxWindow* w){
-    RGA_Set(Time_Nano());
+    Random_Set(Time_Nano());
 
 	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
 	knn = KMeans_New(2U,3U);
